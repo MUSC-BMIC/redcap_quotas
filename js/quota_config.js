a2c3270e10322312faa2878bf25ef5d05c0be611
Andrew Cates <amcates@gmail.com>
2019-10-21 21:58:44 -0400

    working dynamic select and text fields, doesn't work when coming back in

--- js/quota_config.js
+++ js/quota_config.js
@@ -11,13 +11,38 @@ $(document).ready(function() {
     console.log(quotaConfigFields);
 
     $(document).on('change', "select[name*='field-name']", function() {
-      if (quotaConfigFields.hasOwnProperty($(this).val())) {
-        console.log($(this).closest("tr").next("tr").find("td.external-modules-input-td"));
-        console.log($(this).val());
-        console.log(quotaConfigFields[$(this).val()]);
+      selectedVal = $(this).val();
+      if (quotaConfigFields.hasOwnProperty(selectedVal)) {
+        inputTd = $(this).closest("tr").next("tr").find("td.external-modules-input-td")
+        oldInput = inputTd.find("input, select, textarea");
 
-        if (['dropdown', 'radio'].includes(quotaConfigFields[$(this).val()].field_type)) {
+        console.log("Input TD: ", inputTd);
+        console.log("Selected Val: ", selectedVal);
+        console.log("Config for Val: ", quotaConfigFields[selectedVal]);
+
+        // dropdowns and radio buttons
+        if (['dropdown', 'radio'].includes(quotaConfigFields[selectedVal].field_type)) {
+          options = quotaConfigFields[selectedVal].select_choices_or_calculations.split(" | ");
           console.log("Need to convert to select");
+          console.log("Options for Select: ", options);
+
+          newSelect = '<select class="' + oldInput.attr('class') + '" name="' + oldInput.attr('name') + '">';
+
+          $.each(options, function(index, value) {
+            option = value.split(", ");
+            newSelect += '<option value=' + option[0] + '>' + option[1] + '</option>';
+          });
+          newSelect += '</select>';
+
+          oldInput.replaceWith(newSelect);
+        }
+
+        // text, notes, and calculated fields
+        if (['calc', 'text', 'notes'].includes(quotaConfigFields[selectedVal].field_type)) {
+          console.log("Need to convert to input");
+
+          newInput = '<input type="text" class="' + oldInput.attr('class') + '" name="' + oldInput.attr('name') + '">';
+          oldInput.replaceWith(newInput);
         }
       }
     });
