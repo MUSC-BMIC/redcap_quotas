3a15e77ea19ea351c91d89f765dff14ab283686f
Johanna Wiel <jwiel86@gmail.com>
2019-11-13 08:56:54 -0500

    logic changes and comments added

--- QuotaConfig.php
+++ QuotaConfig.php
@@ -56,10 +56,10 @@ class QuotaConfig extends \ExternalModules\AbstractExternalModule
     $config = $this->getProjectSettings();
 
     $total_n_met = $this->total_n_quota_met($config);
-    $generic_quotas_met = $this->generic_quotas_met($config, $params);
+    $generic_quotas_newly_violated = $this->generic_quotas_newly_violated($config, $params);
 
-    // $quota_met = $total_n_met || $dob_quota;
-    $quota_met = $total_n_met || $generic_quotas_met;
+    // Consider quota met if total n quota met or any generic quota is met
+    $quota_met = $total_n_met || $generic_quotas_newly_violated;
     return $quota_met;
   }
 
@@ -72,7 +72,7 @@ class QuotaConfig extends \ExternalModules\AbstractExternalModule
     return ($total_n_enforced == true) && ($total_data_count >= $total_n);
   }
 
-  function generic_quotas_met($config, $request_params)
+  function generic_quotas_newly_violated($config, $request_params)
   {
     $params = array('return_format' => 'array');
     $data = REDCap::getData($params);
@@ -86,72 +86,122 @@ class QuotaConfig extends \ExternalModules\AbstractExternalModule
 
     $event_id = intval($request_params['event_id']);
 
-    $quotas_met = false;
-
+    // Iterate through all generic quotas
     for ($i = 0; $i < count($field_names); $i++)
     {
+      // The property on the record to enforce a quota on (example: `sex`, `age)
       $field_name = $field_names[$i][0];
+
+      // The arithmetic operator to use when determining if the quotas's been met
+      // (value will be one of `=`, `<`, `<=`, `<>`, `>=`, `>`)
       $field_operator = $field_operators[$i];
+
+      // Determines whether to enforce the quota based on the number of matching
+      // records or based on the percentage of total records that match (value
+      // will either be `total` or `%`)
       $field_quantifier = $field_quantifiers[$i];
+
+      // The target number or percent that is the limit for the current quota
       $field_quantity = intval($field_quantities[$i]);
+
+      // The value to match records against (example: `Female`, 15)
       $field_selected = $fields_selected[$i][0];
+
       $matching_records = 0;
 
-      foreach ($data as $record)
+      // Iterate through all existing records to find how many match the current quota
+      foreach ($data as $event_record)
       {
-        $record = $record[$event_id];
+        // Actual record is nested under the event Id
+        $record = $event_record[$event_id];
+
+        // Count the records that have the same value for the specified field as the quota 
         if ($record[$field_name] == $field_selected)
         {
             $matching_records++;
         }
       }
 
+      // By default, use the number of matching records to evaluate if the quota has been met
+      $previous_operand = $matching_records;
+      $new_operand = $matching_records;
+
+      // Need to also account for the data in the request to see if the quota is violated by the
+      // addition of the new record.
       if ($request_params[$field_name] == $field_selected)
       {
-        $matching_records++;
+        $new_operand += 1;
       }
 
-      $operand = $matching_records;
+      // If the quantifier for the current quota is `%`, use the number of matching records
+      // as a percentage of the total number of records to evaluate if the quota has been met
       if ($field_quantifier == '%')
       {
-        $operand = $matching_records / $total_count;
+        $previous_operand = $previous_operand / $total_count;
+        $new_operand = $new_operand / $total_count;
       }
 
-      $quota_met = false;
+      // Need to determine if the quota was already violated before the addition of the new
+      // record as well as whether it will be violated with the addition of the new reford. This
+      // will stop us from incorrectly preventing new records in situations where, for example,
+      // the quota is "at least 10 women" and we currently have 6 women 
+      // (so techincally this would be violating the quoata). We wouldn't want to
+      // stop the user from adding more women then because they're getting closer to fulfilling
+      // the quota.
+      $quota_previously_violated = false;
+      $quota_now_violated = false;
+
       if ($field_operator == '=')
       {
-        $quota_met = ($operand >= $field_quantity);
+        // This value denotes whether the quota was violated just by the data already in the database
+        $quota_previously_violated = ($previous_operand != $field_quantity);
+
+        // This value denotes whether the quota will be violated by adding the new request data to
+        // that already in the database
+        $quota_now_violated = ($new_operand != $field_quantity);
       }
 
       if ($field_operator == '>')
       {
-        $quota_met = ($operand < $field_quantity);
+        $quota_previously_violated = ($previous_operand <= $field_quantity);
+        $quota_now_violated = ($new_operand <= $field_quantity);
       }
 
       if ($field_operator == '>=')
       {
-        $quota_met = ($operand <= $field_quantity);
+        $quota_previously_violated = ($previous_operand < $field_quantity);
+        $quota_now_violated = ($new_operand < $field_quantity);
       }
 
       if ($field_operator == '<')
       {
-        $quota_met = ($operand > $field_quantity);
+        $quota_previously_violated = ($previous_operand >= $field_quantity);
+        $quota_now_violated = ($new_operand >= $field_quantity);
       }
 
       if ($field_operator == '<=')
       {
-        $quota_met = ($operand >= $field_quantity);
+        $quota_previously_violated = ($previous_operand > $field_quantity);
+        $quota_now_violated = ($new_operand > $field_quantity);
       }
 
       if ($field_operator == '<>')
       {
-        $quota_met = ($operand != $field_quantity);
+        $quota_previously_violated = ($previous_operand == $field_quantity);
+        $quota_now_violated = ($new_operand == $field_quantity);
       }
 
-      $quotas_met = ($quotas_met or $quota_met);
+      // This value tracks if the quota was NOT violated by the data existing in the database
+      // but IS violated by the addition of the request data to that of the data in the database
+      $quota_newly_violated = !$quota_previously_violated and $quota_now_violated;
+
+      if ($quota_newly_violated)
+      {
+        return true;
+      }
     }
 
-    return $quotas_met;
+    return false;
   }
 
   protected function dataCount($included_in_quota_n) {
