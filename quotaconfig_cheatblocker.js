a3142f43e0e58566e10610fbdd301d16b4ff9530
royyesud@musc.edu <royyesud@musc.edu>
2020-08-13 16:46:44 -0400

    Additional changes when combining both the modules

--- quotaconfig_cheatblocker.js
+++ quotaconfig_cheatblocker.js
@@ -25,7 +25,9 @@ setTimeout(function() {
           cheat_eligibility_message = data.cheat_eligibility_message;
           potential_duplicate_message = data.potential_duplicate_message;
           failed_criteria = data.failed_criteria;
+          duplicates_count = data.duplicates_count;
           duplicate_record_ids = data.duplicate_record_ids;
+          potential_duplicate_record_ids = data.potential_duplicate_record_ids;
           data_entry_time = data.data_entry_time;
 
           console.log(data);
@@ -67,7 +69,9 @@ setTimeout(function() {
           //save cheat_blocker data
           $("#duplicate_check-tr .data :input").val(is_duplicate);
           $("#failed_criteria-tr .data :input").val(failed_criteria);
+          $("#duplicates_count-tr .data :input").val(duplicates_count);
           $("#duplicate_record_ids-tr .data :input").val(duplicate_record_ids);
+          $("#pot_duplicate_record_ids-tr .data :input").val(potential_duplicate_record_ids);
           if(data_entry_time){
             $("#data_entry_time-tr .data :input").val(data_entry_time);
           }
